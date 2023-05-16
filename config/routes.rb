0fae451df5e2ffecfa3ec3eb0fb6bc298f75a016
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-16 19:28:04 +0900

    デプロイ2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: "items#index" #これ忘れたらサーバーがYearになる｡
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
