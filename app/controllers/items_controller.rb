9ae60ab88a372c64e058da623228c79fc0419b76
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-29 13:06:15 +0900

    ｢出品する｣押下時のページ遷移(ログイン時:出品ページ　未ログイン時:ログイン画面)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,19 @@
 class ItemsController < ApplicationController
+  # before_action :move_to_new, except: [:new]
+
   def index
     @items = Item.all
+    end
   end
-end
+
+  def new
+    @items = Tweet.new
+  end
+
+  def move_to_new
+    unless user_signed_in?
+      redirect_to new_item
+    else
+      redirect_to user_session_path
+    end
+  end
