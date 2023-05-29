6b1ad5765c19fa7c9305ad7ec452fee5a99a18a5
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-29 20:54:26 +0900

    発送元地域の実装(プルダウン)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,6 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :charge
+  belongs_to :area
   validates :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :shippingday_id, :price, presence: true
 end
