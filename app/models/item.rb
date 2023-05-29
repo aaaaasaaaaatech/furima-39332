13262ba49c661bd0f137d7071735d42093f84557
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-29 20:37:04 +0900

    商品状態の実装(プルダウン)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :charge
   validates :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :shippingday_id, :price, presence: true
 end
