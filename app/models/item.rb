df615e6e5e42d2a2babead6de5e6e6d886f564c7
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-29 20:58:58 +0900

    発送までの日数の実装(プルダウン)

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,6 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :charge
   belongs_to :area
+  belongs_to :shippingday
   validates :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :shippingday_id, :price, presence: true
 end
