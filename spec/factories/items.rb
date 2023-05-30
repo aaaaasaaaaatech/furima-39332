eb165cbbe801044c184d3711200be50b5677dcc5
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-30 17:56:32 +0900

    テストコード実行

---
+++ spec/factories/items.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :item do
+    name            {Faker::Lorem.sentence}
+    explanation     {Faker::Lorem.sentence}
+    category_id     { 1 }
+    condition_id    { 1 }
+    charge_id       { 1 }
+    area_id         { 1 }
+    shippingday_id  { 1 }
+    price           { 400 }
+    
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
