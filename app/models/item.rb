3992f92c762031e4a48060085d8e861213fff18b
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-30 15:46:17 +0900

    画像必須のバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :shippingday
   has_one_attached :image
-  validates :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :shippingday_id, presence: true
+  validates :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :shippingday_id, :image, presence: true
   validates :price,presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
