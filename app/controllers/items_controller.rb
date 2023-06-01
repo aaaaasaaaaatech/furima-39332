e3771cfc6d490de49f268bbc32edfd8739c68d13
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-06-01 21:40:34 +0900

    編集ページ 入力済み項目の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   def index
     @items = Item.all
-    charge_id_names= { 2 => '着払い(購入者負担)', 3 => '送料込み(出品者負担)' } 
-    @charge_id_names = charge_id_names
     # @sold_out_items = calculate_sold_out_items(@items)
   end
 
@@ -25,7 +23,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @username = @item.user.nickname
   end
-
+ 
+  def edit
+    @item = Item.find(params[:id])
+  end
 
   def move_to_new
     if user_signed_in?
