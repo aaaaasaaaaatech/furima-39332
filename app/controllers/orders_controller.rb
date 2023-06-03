69c30f70c21e101529ae52550f033ba41ebdb206
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-06-03 11:07:16 +0900

    ログイン状態､出品者による購入ページへの遷移制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,14 @@
 class OrdersController < ApplicationController
   include ActiveModel::Model
   attr_accessor :item, :user
+  before_action :authenticate_user!, only: [:index]
 
   def index
     @order = Order.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id 
+      redirect_to root_path
+    end
   end
 
   def create
