3fb9c52a3f6f7ef811c3ec2db44e050a6c0ac476
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-06-04 17:10:43 +0900

    RuboCop

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe OrderAddress, type: :model do
 
   describe '購入機能テスト' do
     context '購入できる場合' do
-      it 'postcode､area_id､municipalities､address､building､phoneが存在すれば購入できる' do
+      it 'postcode､area_id､municipalities､address､building､phone,number-form,expiry-form,cvc-form(トークンが生成されていれば)が存在すれば購入できる' do
         expect(@order_address).to be_valid
       end
 
@@ -18,6 +18,12 @@ RSpec.describe OrderAddress, type: :model do
     end
 
     context '購入できない場合' do
+      it 'postcodeが空では購入できない' do
+        @order_address.postcode = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postcode can't be blank")
+      end
+
       it 'postcodeが空では購入できない' do
         @order_address.postcode = nil
         @order_address.valid?
@@ -51,33 +57,32 @@ RSpec.describe OrderAddress, type: :model do
       it 'postcodeは3桁ハイフン4桁がないと購入できない' do
         @order_address.postcode = '1234567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
+        expect(@order_address.errors.full_messages).to include('Postcode is invalid. Enter it as follows (e.g. 123-4567)')
       end
 
       it 'postcodは全角では購入できない' do
         @order_address.postcode = '１２３－４５６７'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
+        expect(@order_address.errors.full_messages).to include('Postcode is invalid. Enter it as follows (e.g. 123-4567)')
       end
 
       it 'phoneは10桁以上11桁以内でないと購入できない' do
         @order_address.phone = '123456789'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is too short")
+        expect(@order_address.errors.full_messages).to include('Phone number is too short')
       end
 
       it 'phoneは半角数でのみでないと購入できない' do
         @order_address.phone = '１２３４５６７８９０'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is too short")
+        expect(@order_address.errors.full_messages).to include('Phone number is too short')
       end
 
       it 'phoneはハイフンが入ると購入できない' do
         @order_address.phone = '090-1236-4567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is too short")
+        expect(@order_address.errors.full_messages).to include('Phone number is too short')
       end
-
     end
   end
 end
